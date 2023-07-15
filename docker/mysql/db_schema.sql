df54eafc2b6adebda5bea052658cd2add4910493
hyunkoes <201702083@o.cnu.ac.kr>
2023-07-15 19:02:25 +0900

    Feat: initialize database schema

---
+++ docker/mysql/db_schema.sql
@@ -0,0 +1,55 @@
+create table user(
+    email varchar(100),
+    -- hashed password
+    password varchar(60),
+    user_name varchar(30),
+    photo int,
+    primary key (email)
+);
+create table movie(
+    movie_id int,
+    primary key(movie_id),
+    is_adult bool default false,
+    original_title varchar(100) not null,
+    kr_title varchar(100),
+    poster_path varchar(255),
+    release_data date not null,
+    overview varchar(255)
+);
+create table channel(
+    channel_id int auto_increment,
+    primary key (channel_id),
+    movie_id int,
+    foreign key(movie_id) references movie(movie_id),
+    thread_count int default 0,
+    subscribe_count int default 0,
+    like_count int default 0,
+);
+create table thread(
+    thread_id int auto_increment,
+    primary key(thread_id),
+    channel_id int,
+    content text,
+    foreign key(channel_id) references channel(channel_id),
+    user_id varchar(50),
+    foreign key(user_id) references user(user_id),
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp,
+);
+create table genre(
+    movie_id int,
+    foreign key(movie_id) references movie(movie_id),
+    genre_id int not null
+);
+create table user_subscribe(
+    user_id int,
+    foreign key(user_id) references user(user_id),
+    channel_id int,
+    foreign key(channel_id) references channel(channel_id)
+);
+create table user_scrap(
+    user_id int,
+    foreign key(user_id) references user(user_id),
+    movie_id int,
+    foreign key(movie_id) references movie(movie_id)
+);
