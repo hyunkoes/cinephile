155b685e8ba3ba273a9182258ff6226d062fd11e
hyunkoes <201702083@o.cnu.ac.kr>
2023-08-01 17:24:51 +0900

    feat: Get thread list flow

---
+++ modules/api/thread.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"errors"
+
+	ErrChecker "cinephile/modules/errors"
+	"cinephile/modules/storage"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetThreads(c *gin.Context) ([]Thread, error) {
+	db := storage.DB()
+	var length int
+	_ = db.QueryRow(`select count(*) from thread`).Scan(&length)
+	if length == 0 {
+		return []Thread{}, errors.New("Nothing to show")
+	}
+	rows, err := db.Query(`select * from thread`)
+	if err := ErrChecker.Check(err); err != nil {
+		return []Thread{}, err
+	}
+	defer rows.Close()
+	Threads := make([]Thread, 0)
+	var thread Thread
+	for rows.Next() {
+		_ = rows.Scan(&thread.Thread_id, &thread.Channel_id, &thread.Content,
+			&thread.Email, &thread.Created_at, &thread.Updated_at)
+	}
+	if err := ErrChecker.Check(err); err != nil {
+		if err != nil {
+			return []Thread{}, err
+		}
+		Threads = append(Threads, thread)
+	}
+	return Threads, nil
+}
