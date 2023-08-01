2be91bfacfa1095dde7a22dcd16118c247acd36c
hyunkoes <201702083@o.cnu.ac.kr>
2023-08-01 17:38:29 +0900

    feat: regist thread api handler

--- modules/server/apiHandler.go
+++ modules/server/apiHandler.go
@@ -15,3 +15,21 @@ func getThreads(c *gin.Context) {
 		c.JSON(200, gin.H{"error": nil, "projects": posts})
 	}
 }
+func registThread(c *gin.Context) {
+	err := RegistThread(c)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(200, gin.H{"error": nil})
+	}
+}
+
+// Movie R
+func getMovie(c *gin.Context) {
+	movie, err := GetMovie(c)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(200, gin.H{"error": nil, "movie": movie})
+	}
+}
