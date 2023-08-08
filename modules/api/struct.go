4ed8cb5adc7539742fd3e48b9436e83d52bc651d
hyunkoes <201702083@o.cnu.ac.kr>
2023-08-08 12:20:17 +0900

    feat: Thread struct with recommend state

--- modules/api/struct.go
+++ modules/api/struct.go
@@ -11,3 +11,13 @@ type Thread struct {
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
 }
+type Thread_recommend struct {
+	Thread_id    int       `json:"thread_id"`
+	Channel_id   int       `json:"channel_id"`
+	Content      string    `json:"content"`
+	Email        string    `json:"email"`
+	Parent       int       `json:"parent"`
+	Created_at   time.Time `json:"created_at"`
+	Updated_at   time.Time `json:"updated_at"`
+	Is_recommend bool      `json:"is_recommend"`
+}
