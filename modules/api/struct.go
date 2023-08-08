a47f1479dcce1ab5daa9eeaa4dc593500fc1a8ac
hyunkoes <201702083@o.cnu.ac.kr>
2023-08-08 12:16:59 +0900

    feat: add parent column

--- modules/api/struct.go
+++ modules/api/struct.go
@@ -7,6 +7,7 @@ type Thread struct {
 	Channel_id int       `json:"channel_id"`
 	Content    string    `json:"content"`
 	Email      string    `json:"email"`
+	Parent     int       `json:"parent"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
 }
