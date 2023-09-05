83b7f5f13444aced30260886bf7aee7e95265299
hyunkoes <201702083@o.cnu.ac.kr>
2023-09-05 17:15:48 +0900

    feat: Hot movie API

--- modules/api/movie.go
+++ modules/api/movie.go
@@ -16,8 +16,102 @@ import (
 Return 8 hottest movie ordered by count of thread and like during a week.
 Used in : Movie search flow
 */
-func GetHotMovies(c *gin.Context) ([]Movie, error) {
-	return []Movie{}, nil
+func GetHotMovies(c *gin.Context) ([]MovieSearch, error) {
+	db := storage.DB()
+	_ = db
+	query := `
+SELECT
+    m.movie_id,
+    m.is_adult,
+    m.original_title,
+    m.kr_title,
+    m.poster_path,
+    m.release_date,
+    m.overview,
+    c.channel_id,
+    GROUP_CONCAT(DISTINCT g.genre_id),
+    GROUP_CONCAT(DISTINCT g.genre_name)
+FROM
+    movie AS m
+LEFT JOIN
+    genre_relation AS gr ON m.movie_id = gr.movie_id
+LEFT JOIN
+    genre AS g ON gr.genre_id = g.genre_id
+LEFT JOIN
+    channel AS c ON m.movie_id = c.movie_id
+LEFT JOIN
+    thread AS t ON c.channel_id = t.channel_id
+LEFT JOIN
+    thread_recommend AS tr ON t.thread_id = tr.thread_id
+WHERE
+    DATE(t.created_at) >= DATE(NOW() - INTERVAL 1 WEEK)
+GROUP BY
+    m.movie_id, c.channel_id
+ORDER BY
+    COUNT(DISTINCT t.thread_id) DESC,
+    SUM(tr.is_recommended) DESC
+LIMIT
+    8;
+`
+	rows, err := db.Query(query)
+	if err != nil {
+		return []MovieSearch{}, err
+	}
+
+	defer rows.Close()
+	movies := make([]MovieSearch, 0)
+	var mov MovieSearch
+	var original_title sql.NullString
+	var kr_title sql.NullString
+	var poster_path sql.NullString
+	var release_date sql.NullTime
+	var overview sql.NullString
+	var genres_ids string
+	var genres_names string
+	for rows.Next() {
+		err := rows.Scan(&mov.Movie_id, &mov.Is_adult, &original_title,
+			&kr_title, &poster_path, &release_date, &overview, &mov.Channel_id, &genres_ids, &genres_names)
+		genres := make([]Genre, 0)
+
+		g_ids := strings.Split(genres_ids, ",")
+		g_names := strings.Split(genres_names, ",")
+		for i := 0; i < len(g_ids); i++ {
+			id, _ := strconv.Atoi(g_ids[i])
+			genres = append(genres, Genre{Genre_id: id, Genre_name: g_names[i]})
+		}
+		if err != nil {
+			return []MovieSearch{}, err
+		}
+		if !original_title.Valid {
+			mov.Original_title = ""
+		} else {
+			mov.Original_title = original_title.String
+		}
+		if !kr_title.Valid {
+			mov.Kr_title = ""
+		} else {
+			mov.Kr_title = kr_title.String
+		}
+		if !poster_path.Valid {
+			mov.Poster_path = ""
+		} else {
+			mov.Poster_path = poster_path.String
+		}
+		if !release_date.Valid {
+			mov.Release_date = time.Time{}
+		} else {
+			mov.Release_date = release_date.Time
+		}
+		if !overview.Valid {
+			mov.Overview = ""
+		} else {
+			mov.Overview = overview.String
+		}
+		mov.Genres = genres
+		mov.Poster_path = TmdbPosterAPI(mov.Poster_path)
+		movies = append(movies, mov)
+	}
+	return movies, nil
 }
 
 /*
