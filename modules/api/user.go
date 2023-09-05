df0f4b96bee98b865d3716d6e8ef0ba93c3c1eac
hyunkoes <201702083@o.cnu.ac.kr>
2023-09-05 14:42:15 +0900

    chore: add nullable name field

--- modules/api/user.go
+++ modules/api/user.go
@@ -11,12 +11,17 @@ func RegistUserForTest(c *gin.Context) error {
 	var form struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
+		Name     string `json:"name`
 	}
 	err := c.ShouldBind(&form)
 	if err != nil {
 		return err
 	}
-	query := fmt.Sprintf(`insert into user values ("%s", "%s", "%s", %d)`, form.Email, form.Password, form.Email, 1)
+	name := form.Email
+	if form.Name != "" {
+		name = form.Name
+	}
+	query := fmt.Sprintf(`insert into user values ("%s", "%s", "%s", %d)`, form.Email, form.Password, name, 1)
 	db := storage.DB()
 	_, err = db.Exec(query)
 	if err != nil {
