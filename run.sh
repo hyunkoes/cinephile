cca46385ab6a13faac0853cc5560895754fd0086
hyunkoes <201702083@o.cnu.ac.kr>
2023-09-22 16:00:47 +0900

    feat: shell for separate dev env

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+mkdir src/weights
+
+env="$1"
+case "$1" in
+	dev) # dev env up
+        docker compose -f "docker/docker-compose.dev.yaml" up --build cinephile_mysql -d
+        ./check-db-ready.sh $env
+        docker compose -f "docker/docker-compose.dev.yaml" up --build cinephile_server nginx
+	;;
+	deploy)
+		#release env up
+        docker compose -f "docker/docker-compose.prod.yaml" up --build cinephile_mysql -d
+        ./check-db-ready.sh $env
+        docker compose -f "docker/docker-compose.prod.yaml" up --build cinephile_server nginx
+	;;
+    local)
+        docker compose -f "docker/docker-compose.local.yaml" up --build cinephile_mysql -d
+        ./check-db-ready.sh $env
+    ;;
+	down) # env down
+		if [ "$2" == "" ] ; then
+			docker compose -f "docker/docker-compose.yaml" down
+		else
+			echo "-down : No option"
+		fi
+	;;
+	*) # exception
+	echo "'$1' is unknown command + '$2'"
+	;;
+esac
