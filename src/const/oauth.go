0df1c1a28d855494ab1ff43ab4428125565dd0cf
hyunkoes <201702083@o.cnu.ac.kr>
2023-09-24 20:35:30 +0900

    add constant

---
+++ src/const/oauth.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	KAKAO_GET_TOKEN_ID_URL  = "https://kapi.kakao.com/v1/user/access_token_info"
+	KAKAO_GET_INFO_URL      = "https://kapi.kakao.com/v2/user/me"
+	KAKAO_TOKEN_REFRESH_URL = "https://kauth.kakao.com/oauth/token"
+	GOOGLE_GET_TOKEN_ID_URL = ""
+	GOOGLE_GET_INFO_URL     = ""
+)
---
+++ src/const/query.go
@@ -0,0 +1,7 @@
+package constant
+
+const (
+	HOT_MOVIE_COUNT  = 8
+	MAX_MOVIE_COUNT  = 16
+	MAX_THREAD_COUNT = 10
+)
