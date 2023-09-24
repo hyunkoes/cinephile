f598bb9e757e2ccbd45bd9dbb2580332612f8365
hyunkoes <201702083@o.cnu.ac.kr>
2023-09-24 20:37:18 +0900

    feat: token checker middleware

---
+++ src/modules/middleware/token/tokenValidator.go
@@ -0,0 +1,13 @@
+package token
+
+import "github.com/gin-gonic/gin"
+
+func AccessTokenIsValid(c *gin.Context) bool {
+	_, err := c.Cookie(`access_token`)
+	return err != nil
+}
+
+func RefreshTokenIsValid(c *gin.Context) bool {
+	_, err := c.Cookie(`refresh_token`)
+	return err != nil
+}
