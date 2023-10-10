a8c38660493d628b4ca885ce6637c8948cd7280e
hyunkoes <201702083@o.cnu.ac.kr>
2023-10-11 01:05:54 +0900

    feat: TMDB API - Get stillcuts

--- src/modules/tmdb/movie.go
+++ src/modules/tmdb/movie.go
@@ -5,6 +5,7 @@ import (
 	"cinephile/modules/logging"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -48,12 +49,9 @@ func GetTrailer(movie_id int) []Trailer {
 		site := result["site"].(string)
 		key := result["key"].(string)
 		official := result["official"].(bool)
-		fmt.Println(site, key)
 		var url string
 		if strings.ToLower(site) == "youtube" {
-			fmt.Println("Youtube임 ㅋ")
 			url = "https://www.youtube.com/watch?v=" + key
-			fmt.Println(site, key, url)
 		}
 		trailer.Site = site
 		trailer.Key = key
@@ -62,4 +60,31 @@ func GetTrailer(movie_id int) []Trailer {
 		trailers = append(trailers, trailer)
 	}
 	return trailers
+}
+
+func GetStillCut(movie_id int) []string {
+	tmdb_token = os.Getenv("TMDB_TOKEN")
+	url := fmt.Sprintf(`https://api.themoviedb.org/3/movie/%d/images`, movie_id)
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjJlYWFhN2NhNDk4ZjA4ZGM1OWQzNTg5N2VjZWIwOSIsInN1YiI6IjY0YWZiYzg3NmEzNDQ4MDBlYThmNTczOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.96gO_hH6s2BKQt_UDj3Bgp-yuEfufwxaKBf9_TrjQwM")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var payload interface{}
+	_ = json.Unmarshal(body, &payload)
+	jsonData := payload.(map[string]interface{})
+	results, _ := jsonData["backdrops"].([]interface{})
+	stillcuts := make([]string, 0)
+	for _, item := range results {
+		result := item.(map[string]interface{})
+		stillcut := TmdbPosterAPI(result[`file_path`].(string))
+		stillcuts = append(stillcuts, stillcut)
+	}
+	return stillcuts
 }
